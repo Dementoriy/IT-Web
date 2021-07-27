008f3b28ee77a0332d3f7e4d41ddcd4787883f3b
Dmitriy Vedernikov <dim01-02@mail.ru>
2021-07-27 19:38:55 +0300

    add web

---
+++ IT-Web2/web.js
@@ -0,0 +1,9 @@
+function del1(){
+  document.getElementById("id1").remove();
+}
+function del2(){
+  document.getElementById("id2").remove();
+}
+function del3(){
+  document.getElementById("id3").remove();
+}
